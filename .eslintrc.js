ed68556c557606aedca9d17bc6968b208ada44b3
Shaxzod <shaxzod@MacBook-Pro--SHAXA.local>
2024-02-16 17:05:47 +0500

    asdaffgergrtthry

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,24 +24,8 @@ module.exports = {
     '@next/next/no-img-element': 'off',
     'react/no-unescaped-entities': 'off',
     'import/no-anonymous-default-export': 'off',
-
-    // add new line above comment
-    'lines-around-comment': [
-      'error',
-      {
-        beforeLineComment: true,
-        beforeBlockComment: true,
-        allowBlockStart: true,
-        allowClassStart: true,
-        allowObjectStart: true,
-        allowArrayStart: true
-      }
-    ],
-
-    // add new line above return
-    'newline-before-return': 'error',
-
-    // add new line below import
+    "lines-around-comment": "off",
+    "newline-before-return": "off",
     'import/newline-after-import': [
       'error',
       {
